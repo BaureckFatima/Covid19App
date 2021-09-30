0132c1a484efc185a641ae5738b229eb8b34bf70
BaureckFatima <57326085+BaureckFatima@users.noreply.github.com>
2021-09-30 15:56:35 +0400

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,49 @@
+function getData()
+{
+
+    const API_ENDPOINT = "https://pomber.github.io/covid19/timeseries.json";
+
+    fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => showData(data, "Mauritius"))
+    .catch((err) => handleError(err));
+}
+
+function processData(data)
+{
+    var mru = data["Mauritius"];
+    console.log(mru[mru.length - 1]);
+}
+
+function handleError(err)
+{
+    console.log('%c' + err, 'background: #222; color: #bada55');
+}
+
+function extractMauritiusData(data)
+{
+    console.table(data[country]);
+
+}
+
+function showData(data, country)
+{
+    let id_confirm = document.getElementById("confirmed");
+    let id_recovered = document.getElementById("recovered");
+    let id_deaths = document.getElementById("deaths");
+
+    let slice_data = data[country].slice(-1);
+    let confirmed = slice_data[0]["confirmed"];
+    let recovered = slice_data[0]["recovered"];
+    let deaths = slice_data[0]["deaths"];
+    console.log(slice_data);   
+
+    id_confirm.innerHTML = confirmed;
+    id_recovered.innerHTML = recovered;
+    id_deaths.innerHTML = deaths;
+
+}
+
+getData();
+
+   
